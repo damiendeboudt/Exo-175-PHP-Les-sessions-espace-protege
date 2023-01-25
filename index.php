47e5cdc551fe265e6732ea8079eb08cb5dcbdb71
Damien <damien.deboudt@sfr.fr>
2023-01-25 13:51:16 +0100

    ok.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 $_SESSION['password'] = "check";
 
 if($_POST['password'] === $_SESSION['password']) {
-    echo $_SESSION['password'];
+    echo "Voici le contenu protégé si le mot de passe est OK";
 } else {
     echo "Ouuupsss, mot de passe incorect";
 }
